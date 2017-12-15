3cc1c1509dac7c72d40222b237df25d10b5335e3
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-15 12:43:33 -0800

    // MapVC:  Imported CoreLoacation.  Added Instance Variable for Location Manager.  Selt locationManager.delegate to self.  Added extension CLLocationManagerDelegate.

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
     
@@ -16,6 +17,13 @@ class MapVC: UIViewController {
      */
     
     @IBOutlet weak var mapView: MKMapView!
+    
+    /*
+     Instance Variables
+     */
+    
+    var locationManager = CLLocationManager()
+    
     
     /*
      Functions
@@ -27,6 +35,7 @@ class MapVC: UIViewController {
         
         super.viewDidLoad()
         mapView.delegate = self
+        locationManager.delegate = self
         
     } // END View Did Load
     
@@ -42,6 +51,10 @@ class MapVC: UIViewController {
 } // END Class.
 
 extension MapVC: MKMapViewDelegate {
+    
+}
+
+extension MapVC: CLLocationManagerDelegate {
     
 }
 
