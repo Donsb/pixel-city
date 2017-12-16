d0764c95e579785e138b0abf30ecde9b90f26aea
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-16 15:31:44 -0800

    // PopVC: Created new ViewController class PopVC.  Added new VC to Storyboard.  Set class to PopVC and Identifier as PopVC.

---
+++ pixel-city/Controller/PopVC.swift
@@ -0,0 +1,50 @@
+//
+//  PopVC.swift
+//  pixel-city
+//
+//  Created by Donald Belliveau on 2017-12-16.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController {
+    
+    /*
+     Functions
+     */
+    
+    
+    /* View Did Load Function. */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    } // END View Did Load.
+    
+    
+    
+    
+}
+
+
+
+// PopVC: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
